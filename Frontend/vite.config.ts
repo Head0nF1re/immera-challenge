c66fbe3fadcab8617c0c6c8a4e1371076b6b6ada
Head0nF1re <77078775+Head0nF1re@users.noreply.github.com>
2024-11-15 16:19:12 +0000

    Initial Vue setup

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,36 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+    server: {
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
+    watch: {
+      usePolling: true,
+    },
+    host: '0.0.0.0',
+    port: 5173,
+  },
+})
