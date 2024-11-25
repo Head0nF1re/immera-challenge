46b4517ba9aba27b478fc1c8e461498964071acf
Head0nF1re <77078775+Head0nF1re@users.noreply.github.com>
2024-11-25 18:13:30 +0000

    Improve pagination
    - Make pagination work using the DataTable Paginator and the browser
      back/forward button.

--- Frontend/src/api/productApi.ts
+++ Frontend/src/api/productApi.ts
@@ -1,7 +1,6 @@
 import type { IPaginate } from '@/types/api/paginationTypes'
 import type { IProduct } from '@/types/api/productApiTypes'
 import httpClient from '@/utils/axios'
-import type { Ref } from 'vue'
 
 export const getAllProducts = async (pageNumber: number) => {
   return await httpClient.get<IPaginate<IProduct>>(`products?page=${pageNumber}`)
@@ -17,8 +16,4 @@ export const updateProduct = async (updateRequest: IProduct) => {
 
 export const deleteProduct = async (id: number) => {
   return await httpClient.post(`/products/${id}`)
-}
-
-export const productApi = {
-  getAllProducts,
 }
--- Frontend/src/views/ProductListView.vue
+++ Frontend/src/views/ProductListView.vue
@@ -3,18 +3,31 @@ import { getAllProducts } from '@/api/productApi';
 import BaseLayout from '@/layouts/BaseLayout.vue';
 import { useQuery } from '@tanstack/vue-query';
 import type { PageState } from 'primevue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-const pageNumber = ref(1)
+const route = useRoute()
+const router = useRouter()
+const pageQuery = typeof route.query.page === 'string' ? parseInt(route.query.page) : 1
+const pageNumber = ref(pageQuery)
+
+watch(
+    () => route.query,
+    (newQuery) => {
+        console.log(newQuery)
+        const pageQuery = typeof newQuery.page === 'string' ? parseInt(newQuery.page) : 1
+        pageNumber.value = pageQuery
+    }
+)
 
 const { data: response, isLoading, refetch } = useQuery({
     queryKey: ['products', pageNumber],
     queryFn: () => getAllProducts(pageNumber.value),
-
 })
 
 const onChangePage = async (e: PageState) => {
     pageNumber.value = e.page + 1
+    router.push({ name: route.name, query: { page: pageNumber.value } })
 }
 
 const onRefresh = async () => {
@@ -28,7 +41,7 @@ const onRefresh = async () => {
         <div class="card flex justify-center">
             <DataTable :value="response?.data.data" data-key="id" paginator :rows="response?.data.per_page"
                 :total-records="response?.data.total" tableStyle="min-width: 50rem" class="w-3/4" @page="onChangePage"
-                :lazy="true" :loading="isLoading && !response">
+                :lazy="true" :loading="isLoading && !response" :first="response?.data.from as number">
                 <template #header>
                     <div class="flex flex-wrap items-center justify-between gap-2">
                         <span class="text-xl font-bold">Products</span>
